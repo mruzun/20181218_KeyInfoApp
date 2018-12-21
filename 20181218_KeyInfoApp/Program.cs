5103943b81d7112416075e5b4175446f52fc841b
Emre Uzun <emreuzun2014@gmail.com>
2018-12-21 23:17:03 +0300

    Initial commit

---
+++ 20181218_KeyInfoApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20181218_KeyInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            top:
+            // Konsolda tuşlanacak veriyi saklayabilmek için ConsoleKeyInfo tipinde bir değişken tanımlanır.
+            ConsoleKeyInfo cki = Console.ReadKey();
+            // Basılan tuşun esc olup olmadığı kontrolü yapılır.
+            if (cki.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(1);
+            }
+            // Basılan tuş f2 olup olmadığını kontrol ediyor
+            else if (cki.Key== ConsoleKey.F2)
+            {
+                goto top;
+            }
+        }
+    }
+}
